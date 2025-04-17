c776d6de3201034091e4e4f0234cadd86c71fad4
joaquinharguindeguy <jaharguindeguy@mail.austral.edu.ar>
2025-04-17 18:19:48 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,7 +1,7 @@
 import math
 def line():
     a = float(input("Ingrese coeficiente A: "))
-    b = float(input("Ingrese Coeficiente B: "))
+    b = float(input("Ingrese coeficiente B: "))
     x1 = float(input("Ingrese el coeficiente X1:"))
     x2 = float(input("Ingrese el coeficiente X2:"))
     print(f'El coeficiente A de su ecuacion es: {a}')
@@ -15,4 +15,4 @@ def line():
     C1= P1 - P2
     C2= x1 - x2
     c= math.sqrt(C1**2 + C2**2)
-    print(f'\nLa distancia entre ellos es:{c}')
+    print(f'\nLa distancia entre ellos es: {c}')
